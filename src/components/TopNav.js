0e21306374bc8c54dc5ad25c322fc6e64276bad1
jwo0o0 <jwo0o0358@gmail.com>
2023-03-25 22:56:01 +0900

    상단 navbar 수정

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,28 +1,39 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { HiSearch } from "react-icons/hi";
 import { FaUser } from "react-icons/fa";
 import { AiFillHeart } from "react-icons/ai";
+import { ScrapModal } from "./ScrapModal";
 
 export const TopNav = () => {
+  const [isOpenScrapmodal, setIsOpenScrapmodal] = useState(false);
+
   return (
-    <TopNavContainer>
-      <MainLogo className="logo">내일</MainLogo>
-      <SearchbarContainer>
-        <input type="text"></input>
-        <HiSearch className="search" />
-      </SearchbarContainer>
-      <NavBtn className="like">
-        <AiFillHeart />
-      </NavBtn>
-      <NavBtn className="mypage">
-        <FaUser />
-      </NavBtn>
-    </TopNavContainer>
+    <>
+      <TopNavContainer>
+        <MainLogo className="logo">내일</MainLogo>
+        <SearchbarContainer>
+          <input type="text"></input>
+          <HiSearch className="search" />
+        </SearchbarContainer>
+        <NavBtn
+          className="like"
+          onClick={() => {
+            setIsOpenScrapmodal(!isOpenScrapmodal);
+          }}
+        >
+          <AiFillHeart />
+        </NavBtn>
+        <NavBtn className="mypage">
+          <FaUser />
+        </NavBtn>
+      </TopNavContainer>
+      <ScrapModal isOpen={isOpenScrapmodal} setIsOpen={setIsOpenScrapmodal} />
+    </>
   );
 };
 
 const TopNavContainer = styled.div`
-  border-bottom: 1px solid #d9d9d9;
   box-sizing: border-box;
   width: 100%;
   height: 80px;
@@ -42,11 +53,13 @@ const SearchbarContainer = styled.div`
   input {
     width: 210px;
     height: 36px;
-    background: #f5f5f5;
-    border: none;
+    border: 1px solid #f5f5f5;
     border-radius: 20px;
     outline: none;
     padding: 0 30px 0 15px;
+    @media screen and (max-width: 400px) {
+      width: 180px;
+    }
   }
   .search {
     position: absolute;
@@ -55,6 +68,7 @@ const SearchbarContainer = styled.div`
     &:hover {
       cursor: pointer;
     }
+    fill: #d9d9d9;
   }
 `;
 
@@ -62,8 +76,8 @@ const NavBtn = styled.button`
   background-color: transparent;
   border: none;
   svg {
-    width: 24px;
-    height: 24px;
+    width: 20px;
+    height: 20px;
     fill: #d9d9d9;
   }
   &:hover {
@@ -71,7 +85,7 @@ const NavBtn = styled.button`
   }
   position: absolute;
   &.mypage {
-    right: 1%;
+    right: 2%;
   }
   &.like {
     right: 10%;
